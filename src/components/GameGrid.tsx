6e35a7f4c6b8a9b87aa643ee1d2b2e52c747271f
sabanikoladze <saba@datamind.ge>
2024-09-21 15:03:35 +0400

    refactor gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,27 +15,26 @@ const GameGrid = ({ gameQuery }: Props) => {
 
   const Skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text> {error}</Text>;
+
   return (
-    <>
-      {error && <Text> {error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          Skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((data) => (
-          <GameCardContainer key={data.id}>
-            <GameCard game={data} />{" "}
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        Skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((data) => (
+        <GameCardContainer key={data.id}>
+          <GameCard game={data} />{" "}
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
