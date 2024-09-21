06dc51a20b2ae8ff7b5fb170f674dee88a93f7b0
sabanikoladze <saba@datamind.ge>
2024-09-21 14:03:24 +0400

    clean up the genres

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
       }}
       templateColumns={{
         base: "1fr", //1 fraction
-        lg: "260px 1fr",
+        lg: "200px 1fr",
       }}
     >
       <GridItem area="nav">
