d4f4fedf2f3d4375a45e177a59efeac9e8afb5c0
sabanikoladze <saba@datamind.ge>
2024-09-21 15:20:37 +0400

    gamegrid error fix

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid = ({ gameQuery }: Props) => {
 
   const Skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text> {error}</Text>;
+  if (!error) return <Text> {error}</Text>;
 
   return (
     <SimpleGrid
